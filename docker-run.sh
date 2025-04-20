9e1dcf0db4f9ce19b0fd852467e063546c1fca80
pzoli <papp.zoltan@integrity.hu>
2025-04-20 19:17:29 +0200

    docker run fileinfo with mongo

---
+++ docker-run.sh
@@ -0,0 +1,15 @@
+docker network create my-net
+
+docker run -e "MONGO_INITDB_ROOT_USERNAME=admin" \
+-e "MONGO_INITDB_ROOT_PASSWORD=example" \
+--network my-net --net-alias=mongoserver \
+-p 27017:27017 -t mongo:latest
+
+
+docker run -e "MONGODB_USER=admin" \
+-e "MONGODB_PASSWORD=example" \
+-e "MONGODB_HOST=mongoserver" \
+-e "MONGODB_PORT=27017" \
+-e "MONGODB_DATABASE=fileinfo" \
+--network my-net \
+ -p 8080:8080 -t pzoli77/fileinfo:latest
